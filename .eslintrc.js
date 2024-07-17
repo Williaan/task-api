7ebb2427be8eadefe71c43298024a5ad323b54fc
Willian <willianmarcos907@gmail.com>
2024-07-16 22:44:35 -0300

    update repository

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,10 @@ module.exports = {
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
+
   rules: {
+
+    'indent': ['error', '2'],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
