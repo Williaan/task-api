9703c06a7f64b36696575809d5ae4750df85cc82
Willian <willianmarcos907@gmail.com>
2024-07-19 23:59:42 -0300

    create user

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,5 +1,5 @@
 import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
-import { TaskDto } from './task.dto';
+import { FindAllParameters, TaskDto } from './task.dto';
 
 @Injectable()
 export class TaskService {
@@ -19,6 +19,25 @@ export class TaskService {
         }
 
         throw new HttpException(`Task with id ${id} not found!`, HttpStatus.NOT_FOUND)
+    }
+
+    findAll(params: FindAllParameters): TaskDto[] {
+        return this.dadosTasks.filter(t => {
+            let match = true;
+
+            if (params.title !== undefined && !t.title.includes(params.title)) {
+                match = false;
+            }
+
+            if (params.status !== undefined && !t.status.includes(params.status)) {
+                match = false;
+            }
+
+
+            return match;
+        })
+
+
     }
 
 
